4e5c7bba545d80feae7528e4f53bb2ebc07967ff
Vigneshvasanth <vigneshvasanth.c@trustrace.com>
2020-07-18 22:09:37 +0530

    life-cycle-hooks component is created

--- life-cycle-hooks/src/app/app-routing.module.ts
+++ life-cycle-hooks/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LifeCycleHooksComponent } from './life-cycle-hooks/life-cycle-hooks.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'life-cycle-hooks', component:LifeCycleHooksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
