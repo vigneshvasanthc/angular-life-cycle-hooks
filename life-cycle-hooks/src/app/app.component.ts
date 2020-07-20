0409938b2ae860ab69401a0f204463833f6d08fc
Vigneshvasanth <vigneshvasanth.c@trustrace.com>
2020-07-20 18:10:03 +0530

    ngOnchange ngOnInit ngDestroy method is deployed

--- life-cycle-hooks/src/app/app.component.ts
+++ life-cycle-hooks/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'life-cycle-hooks';
+  
+  name = 'Angular';
+
+  show = false;
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('component initialized')
+  }
+
+  toggle() {
+    this.show = !this.show;
+  }
 }
--- life-cycle-hooks/src/app/life-cycle-hooks/life-cycle-hooks.component.ts
+++ life-cycle-hooks/src/app/life-cycle-hooks/life-cycle-hooks.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-life-cycle-hooks',
   templateUrl: './life-cycle-hooks.component.html',
   styleUrls: ['./life-cycle-hooks.component.scss']
 })
-export class LifeCycleHooksComponent implements OnInit {
+export class LifeCycleHooksComponent implements OnInit, OnDestroy, OnChanges {
+
+  @Input() name: string;
+
+  timeoutInstance = null
 
   constructor() { }
 
+  ngOnChanges(changes) {
+    console.log('on changes', changes)
+  }
+
   ngOnInit() {
+    console.log('component initialized');
+
+    /* this.timeoutInstance = setInterval(() => {
+      console.log(new Date());
+    }, 1000); */
+  }
+
+  ngOnDestroy() {
+    console.log('component destroyed');
+
+    if(this.timeoutInstance) {
+      clearInterval(this.timeoutInstance);
+    }
   }
 
 }
